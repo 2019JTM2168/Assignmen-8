3269100568de7f4297ae08651f54eb790731152a
srijan upadhyay <jtm192168@iitd.ac.in>
2019-09-18 10:35:55 +0530

    first commit

--- ps2.py
+++ ps2.py
@@ -1,1 +1,21 @@
 
+
+
+def inp():
+    x=int(input("Enter position : "))  #taking position from user
+    if (x>9 or x<1):
+        print("Invalid position")
+        exit()
+    y=int(input("Enter number : ")) #taking number from user
+    if (y>9 or y<1):
+        print("Invalid number")
+        exit()
+    print (x,y)
+    return x,y
+
+print("Welcome to the game")
+print("Player 1")
+inp()
+int(x)
+if(x==1):
+    a=y
