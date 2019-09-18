635765fe04e4464110b4eab3b7766e4a9df7ef9b
srijan upadhyay <jtm192168@iitd.ac.in>
2019-09-18 11:31:14 +0530

    fourth commit

--- ps2.py
+++ ps2.py
@@ -9,6 +9,14 @@ def inp():
         print("Invalid number")
         exit()
     return x, y
+#
+# def disp():
+#     print(a     b   c)
+#     print()
+#     print(d     e   f)
+#     print()
+#     print(g     h   i)
+
 
 
 # def assgn():
@@ -36,189 +44,217 @@ a, b, c, d, e, f, g, h, i = 0
 print("Welcome to the game")
 print("Player 1")   #First input
 x, y = inp()
-if (x == 1):
+if x == 1:
     a = y
-if (x == 2):
+if x == 2:
     b = y
-if (x == 3):
+if x == 3:
     c = y
-if (x == 4):
+if x == 4:
     d = y
-if (x == 5):
+if x == 5:
     e = y
-if (x == 6):
+if x == 6:
     f = y
-if (x == 7):
+if x == 7:
     g = y
-if (x == 8):
+if x == 8:
     h = y
-if (x == 9):
+if x == 9:
     i = y
+if (((a + b + c)==15) or ((d + e + f)==15) or ((g + h + i)==15) or ((a + d + g)==15) or ((b + e + h)==15) or ((c + f + i)==15) or ((a + e + i)==15) or ((g + e + c)==15)):
+        print("Player 1 won")
+        exit()
 
 print("Player 2")   #Second input
 x, y = inp()
-if (x == 1):
+if x == 1:
     a = y
-if (x == 2):
+if x == 2:
     b = y
-if (x == 3):
+if x == 3:
     c = y
-if (x == 4):
+if x == 4:
     d = y
-if (x == 5):
+if x == 5:
     e = y
-if (x == 6):
+if x == 6:
     f = y
-if (x == 7):
+if x == 7:
     g = y
-if (x == 8):
+if x == 8:
     h = y
-if (x == 9):
+if x == 9:
     i = y
+if (((a + b + c)==15) or ((d + e + f)==15) or ((g + h + i)==15) or ((a + d + g)==15) or ((b + e + h)==15) or ((c + f + i)==15) or ((a + e + i)==15) or ((g + e + c)==15)):
+        print("Player 2 won")
+        exit()
 
 print("Player 1")    #Third input
 x, y = inp()
-if (x == 1):
+if x == 1:
     a = y
-if (x == 2):
+if x == 2:
     b = y
-if (x == 3):
+if x == 3:
     c = y
-if (x == 4):
+if x == 4:
     d = y
-if (x == 5):
+if x == 5:
     e = y
-if (x == 6):
+if x == 6:
     f = y
-if (x == 7):
+if x == 7:
     g = y
-if (x == 8):
+if x == 8:
     h = y
-if (x == 9):
+if x == 9:
     i = y
+if (((a + b + c)==15) or ((d + e + f)==15) or ((g + h + i)==15) or ((a + d + g)==15) or ((b + e + h)==15) or ((c + f + i)==15) or ((a + e + i)==15) or ((g + e + c)==15)):
+        print("Player 1 won")
+        exit()
 
 print("Player 2")       #fourth input
 x, y = inp()
-if (x == 1):
+if x == 1:
     a = y
-if (x == 2):
+if x == 2:
     b = y
-if (x == 3):
+if x == 3:
     c = y
-if (x == 4):
+if x == 4:
     d = y
-if (x == 5):
+if x == 5:
     e = y
-if (x == 6):
+if x == 6:
     f = y
-if (x == 7):
+if x == 7:
     g = y
-if (x == 8):
+if x == 8:
     h = y
-if (x == 9):
+if x == 9:
     i = y
+if (((a + b + c)==15) or ((d + e + f)==15) or ((g + h + i)==15) or ((a + d + g)==15) or ((b + e + h)==15) or ((c + f + i)==15) or ((a + e + i)==15) or ((g + e + c)==15)):
+        print("Player 2 won")
+        exit()
 
 print("Player 1")   #fifth input
 x, y = inp()
-if (x == 1):
+if x == 1:
     a = y
-if (x == 2):
+if x == 2:
     b = y
-if (x == 3):
+if x == 3:
     c = y
-if (x == 4):
+if x == 4:
     d = y
-if (x == 5):
+if x == 5:
     e = y
-if (x == 6):
+if x == 6:
     f = y
-if (x == 7):
+if x == 7:
     g = y
-if (x == 8):
+if x == 8:
     h = y
-if (x == 9):
+if x == 9:
     i = y
+if (((a + b + c)==15) or ((d + e + f)==15) or ((g + h + i)==15) or ((a + d + g)==15) or ((b + e + h)==15) or ((c + f + i)==15) or ((a + e + i)==15) or ((g + e + c)==15)):
+        print("Player 1 won")
+        exit()
 
-    print("Player 2")   #sixth input
-    x, y = inp()
-    if (x == 1):
-        a = y
-    if (x == 2):
-        b = y
-    if (x == 3):
-        c = y
-    if (x == 4):
-        d = y
-    if (x == 5):
-        e = y
-    if (x == 6):
-        f = y
-    if (x == 7):
-        g = y
-    if (x == 8):
-        h = y
-    if (x == 9):
-        i = y
+print("Player 2")   #sixth input
+x, y = inp()
+if x == 1:
+    a = y
+if x == 2:
+    b = y
+if x == 3:
+    c = y
+if x == 4:
+    d = y
+if x == 5:
+    e = y
+if x == 6:
+    f = y
+if x == 7:
+    g = y
+if x == 8:
+    h = y
+if x == 9:
+    i = y
+if (((a + b + c) == 15) or ((d + e + f) == 15) or ((g + h + i) == 15) or ((a + d + g) == 15) or ((b + e + h) == 15) or (
+        (c + f + i) == 15) or ((a + e + i) == 15) or ((g + e + c) == 15)):
+    print("Player 2 won")
+    exit()
 
 print("Player 1")   #seventh input
 x, y = inp()
-if (x == 1):
+if x == 1:
     a = y
-if (x == 2):
+if x == 2:
     b = y
-if (x == 3):
+if x == 3:
     c = y
-if (x == 4):
+if x == 4:
     d = y
-if (x == 5):
+if x == 5:
     e = y
-if (x == 6):
+if x == 6:
     f = y
-if (x == 7):
+if x == 7:
     g = y
-if (x == 8):
+if x == 8:
     h = y
-if (x == 9):
+if x == 9:
     i = y
+if (((a + b + c)==15) or ((d + e + f)==15) or ((g + h + i)==15) or ((a + d + g)==15) or ((b + e + h)==15) or ((c + f + i)==15) or ((a + e + i)==15) or ((g + e + c)==15)):
+        print("Player 1 won")
+        exit()
 
 print("Player 2")  #Eighth input
 x, y = inp()
-if (x == 1):
+if x == 1:
     a = y
-if (x == 2):
+if x == 2:
     b = y
-if (x == 3):
+if x == 3:
     c = y
-if (x == 4):
+if x == 4:
     d = y
-if (x == 5):
+if x == 5:
     e = y
-if (x == 6):
+if x == 6:
     f = y
-if (x == 7):
+if x == 7:
     g = y
-if (x == 8):
+if x == 8:
     h = y
-if (x == 9):
+if x == 9:
     i = y
+if (((a + b + c)==15) or ((d + e + f)==15) or ((g + h + i)==15) or ((a + d + g)==15) or ((b + e + h)==15) or ((c + f + i)==15) or ((a + e + i)==15) or ((g + e + c)==15)):
+        print("Player 2 won")
+        exit()
 
 print("Player 1")  #ninth input
 x, y = inp()
-if (x == 1):
+if x == 1:
     a = y
-if (x == 2):
+if x == 2:
     b = y
-if (x == 3):
+if x == 3:
     c = y
-if (x == 4):
+if x == 4:
     d = y
-if (x == 5):
+if x == 5:
     e = y
-if (x == 6):
+if x == 6:
     f = y
-if (x == 7):
+if x == 7:
     g = y
-if (x == 8):
+if x == 8:
     h = y
-if (x == 9):
+if x == 9:
     i = y
+if (((a + b + c)==15) or ((d + e + f)==15) or ((g + h + i)==15) or ((a + d + g)==15) or ((b + e + h)==15) or ((c + f + i)==15) or ((a + e + i)==15) or ((g + e + c)==15)):
+        print("Player 1 won")
+        exit()
