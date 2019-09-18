0ad329fbee307cb33ecb889ddc7bf58b192a5f43
srijan upadhyay <jtm192168@iitd.ac.in>
2019-09-18 10:59:11 +0530

    third commit

--- ps2.py
+++ ps2.py
@@ -1,16 +1,146 @@
 def inp():
-    x=int(input("Enter position : "))  #taking position from user
-    if(x>9 or x<1):
+    global x, y
+    x = int(input("Enter position : "))  # taking position from user
+    if (x > 9 or x < 1):
         print("Invalid position")
         exit()
-    y=int(input("Enter number : ")) #taking number from user
-    if(y>9 or y<1):
+    y = int(input("Enter number : "))  # taking number from user
+    if (y > 9 or y < 1):
         print("Invalid number")
         exit()
-    return x,y
+    return x, y
 
-def assgn():
-    global a, b, c, g, f, e, d, i, h
+
+# def assgn():
+#     global a, b, c, g, f, e, d, i, h
+#     if (x == 1):
+#         a = y
+#     if (x == 2):
+#         b = y
+#     if (x == 3):
+#         c = y
+#     if (x == 4):
+#         d = y
+#     if (x == 5):
+#         e = y
+#     if (x == 6):
+#         f = y
+#     if (x == 7):
+#         g = y
+#     if (x == 8):
+#         h = y
+#     if (x == 9):
+#         i = y
+#     return a, b, c, d, e, f, g, h, i
+a, b, c, d, e, f, g, h, i = 0
+print("Welcome to the game")
+print("Player 1")   #First input
+x, y = inp()
+if (x == 1):
+    a = y
+if (x == 2):
+    b = y
+if (x == 3):
+    c = y
+if (x == 4):
+    d = y
+if (x == 5):
+    e = y
+if (x == 6):
+    f = y
+if (x == 7):
+    g = y
+if (x == 8):
+    h = y
+if (x == 9):
+    i = y
+
+print("Player 2")   #Second input
+x, y = inp()
+if (x == 1):
+    a = y
+if (x == 2):
+    b = y
+if (x == 3):
+    c = y
+if (x == 4):
+    d = y
+if (x == 5):
+    e = y
+if (x == 6):
+    f = y
+if (x == 7):
+    g = y
+if (x == 8):
+    h = y
+if (x == 9):
+    i = y
+
+print("Player 1")    #Third input
+x, y = inp()
+if (x == 1):
+    a = y
+if (x == 2):
+    b = y
+if (x == 3):
+    c = y
+if (x == 4):
+    d = y
+if (x == 5):
+    e = y
+if (x == 6):
+    f = y
+if (x == 7):
+    g = y
+if (x == 8):
+    h = y
+if (x == 9):
+    i = y
+
+print("Player 2")       #fourth input
+x, y = inp()
+if (x == 1):
+    a = y
+if (x == 2):
+    b = y
+if (x == 3):
+    c = y
+if (x == 4):
+    d = y
+if (x == 5):
+    e = y
+if (x == 6):
+    f = y
+if (x == 7):
+    g = y
+if (x == 8):
+    h = y
+if (x == 9):
+    i = y
+
+print("Player 1")   #fifth input
+x, y = inp()
+if (x == 1):
+    a = y
+if (x == 2):
+    b = y
+if (x == 3):
+    c = y
+if (x == 4):
+    d = y
+if (x == 5):
+    e = y
+if (x == 6):
+    f = y
+if (x == 7):
+    g = y
+if (x == 8):
+    h = y
+if (x == 9):
+    i = y
+
+    print("Player 2")   #sixth input
+    x, y = inp()
     if (x == 1):
         a = y
     if (x == 2):
@@ -29,13 +159,66 @@ def assgn():
         h = y
     if (x == 9):
         i = y
-    return a, b, c, d, e, f, g, h, i
 
-print("Welcome to the game")
-print("Player 1")
-x,y=inp()
+print("Player 1")   #seventh input
+x, y = inp()
+if (x == 1):
+    a = y
+if (x == 2):
+    b = y
+if (x == 3):
+    c = y
+if (x == 4):
+    d = y
+if (x == 5):
+    e = y
+if (x == 6):
+    f = y
+if (x == 7):
+    g = y
+if (x == 8):
+    h = y
+if (x == 9):
+    i = y
 
-print("Player 2")
-x,y=inp()
-a, b, c, d, e, f, g, h, i = assgn()
+print("Player 2")  #Eighth input
+x, y = inp()
+if (x == 1):
+    a = y
+if (x == 2):
+    b = y
+if (x == 3):
+    c = y
+if (x == 4):
+    d = y
+if (x == 5):
+    e = y
+if (x == 6):
+    f = y
+if (x == 7):
+    g = y
+if (x == 8):
+    h = y
+if (x == 9):
+    i = y
 
+print("Player 1")  #ninth input
+x, y = inp()
+if (x == 1):
+    a = y
+if (x == 2):
+    b = y
+if (x == 3):
+    c = y
+if (x == 4):
+    d = y
+if (x == 5):
+    e = y
+if (x == 6):
+    f = y
+if (x == 7):
+    g = y
+if (x == 8):
+    h = y
+if (x == 9):
+    i = y
