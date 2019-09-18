611ba97508ca477f067e00c5894d6175895c029c
srijan upadhyay <jtm192168@iitd.ac.in>
2019-09-18 14:33:13 +0530

    third commit

--- ps1.py
+++ ps1.py
@@ -1,36 +1,26 @@
-# creating an empty list
-lst = []
+#taking input from user
+x=input("Enter binary data : ")
+n=len(x)    #string length
+count=0
+for i in range(0,n):
+    if x[i] in '0':
+        print()
+    elif x[i] in '1':
+        count +=1       #counting ones
+    else:
+        print("Invalid data")
+        exit()
+p=count%2   #finding parity
+# print(count)
+if p == 0:
+    print("Even parity")
+    print("Data with parity bit : {0}".format(x+"1"))       #printing even parity data to be transmitted
+if p==1:
+    print("Odd parity")
+    print("Data with parity bit : {0}".format(x + "0"))     #printing odd parity data to be transmitted
+#replacing 010 with 0100
+y=x.replace("010","0100")
+print("Modified string to be transmitted : {0}".format(y+"0101"))
 
-# number of elemetns as input
-n = int(input("Enter number of bits : "))
 
-# iterating till the range
-for i in range(1, n):
-    ele = int(input())
 
-    lst.append(ele)  # adding the element
-print(lst)
-
-for i in lst:
-    print(lst[i])
-
-# def convert(string):
-#     li=list(string.split(" "))
-#     return li
-#
-# x=input("Enter binary data :")
-# count=0
-#
-# y=print(convert(x))
-
-# x=x.split('')
-#
-# for items in x
-#     print (i)
-#     if items==1
-#         count++
-#     if count%2==0
-#         p=1
-#     if count%2==1
-#         p=0
-# print(p)
